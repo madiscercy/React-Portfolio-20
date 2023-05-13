c0f32c45e5e63ad5bba9d552762903fa243f6fba
madiscercy <madiscercy@gmail.com>
2023-05-13 17:19:47 -0400

    fleshed out portfolio with all my projects

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -2,18 +2,41 @@ import React from 'react';
 
 const projects = [
 	{
-		name: 'Project 1',
-		image: '/path/to/image1.jpg',
-		deployedUrl: 'https://example.com/project1',
-		repoUrl: 'https://github.com/username/project1',
+		name: 'Bits and Bytes',
+		image: require('../../images/bits-and-bytes.png'),
+		deployedUrl: 'https://bits-and-bytes.herokuapp.com/',
+		repoUrl: 'https://github.com/IsaiahGz/bits-and-bytes',
 	},
 	{
-		name: 'Project 2',
-		image: '/path/to/image2.jpg',
-		deployedUrl: 'https://example.com/project2',
-		repoUrl: 'https://github.com/username/project2',
+		name: 'Simultaneous Searches',
+		image: require('../../images/simultaneous-searches.png'),
+		deployedUrl: 'https://csnyder94.github.io/simultaneous-searches/',
+		repoUrl: 'https://github.com/csnyder94/simultaneous-searches',
 	},
-	// add more projects here
+	{
+		name: 'Note Taker',
+		image: require('../../images/note-taker.png'),
+		deployedUrl: 'https://module-11-note-taker.herokuapp.com/',
+		repoUrl: 'https://github.com/madiscercy/Note-Taker-11',
+	},
+	{
+		name: 'Weather Dashboard',
+		image: require('../../images/weather-dashboard.png'),
+		deployedUrl: 'https://madiscercy.github.io/Weather-Dashboard06/',
+		repoUrl: 'https://github.com/madiscercy/Weather-Dashboard06',
+	},
+	{
+		name: 'E-commerce',
+		image: require('../../images/e-commerce.png'),
+		deployedUrl: 'https://drive.google.com/file/d/1m3FZ3JD9BR4C4dxjSomOx3iI8lE1kWs3/view',
+		repoUrl: 'https://github.com/madiscercy/E-Commerce-Back-End-13',
+	},
+	{
+		name: 'Code Quiz',
+		image: require('../../images/code-quiz.png'),
+		deployedUrl: 'https://madiscercy.github.io/Code-Quiz-Challenge04/',
+		repoUrl: 'https://github.com/madiscercy/Code-Quiz-Challenge04',
+	},
 ];
 
 const Portfolio = () => {
@@ -25,11 +48,11 @@ const Portfolio = () => {
 					<div key={index} className='border p-2'>
 						<img src={project.image} alt={project.name} className='w-full' />
 						<h3 className='font-bold mt-2'>{project.name}</h3>
-						<a href={project.deployedUrl} target='_blank' rel='noreferrer' className='text-blue-500 hover:text-blue-800'>
+						<a href={project.deployedUrl} target='_blank' rel='noreferrer' className='text-gray-500 hover:text-gray-800'>
 							Deployed Application
 						</a>
 						<br />
-						<a href={project.repoUrl} target='_blank' rel='noreferrer' className='text-blue-500 hover:text-blue-800'>
+						<a href={project.repoUrl} target='_blank' rel='noreferrer' className='text-gray-500 hover:text-gray-800'>
 							GitHub Repository
 						</a>
 					</div>
