2c1cd1aa27fe42a39e84875a06e0ecba7d80f783
madiscercy <madiscercy@gmail.com>
2023-05-13 11:15:35 -0400

    finished contact page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 // import About from './components/About/About';
 import Portfolio from './components/Portfolio/Portfolio';
-// import Contact from './components/Contact/Contact';
+import Contact from './components/Contact/Contact';
 // import Resume from './components/Resume/Resume';
 import Footer from './components/Footer/Footer';
 import './output.css';
@@ -15,8 +15,8 @@ function App() {
 			<Routes>
 				{/* <Route path='/' element={<About />} /> */}
 				<Route path='/portfolio' element={<Portfolio />} />
-				{/* <Route path='/contact' element={<Contact />} />
-				<Route path='/resume' element={<Resume />} /> */}
+				<Route path='/contact' element={<Contact />} />
+				{/* <Route path='/resume' element={<Resume />} /> */}
 			</Routes>
 			<Footer /> {Footer}
 		</Router>
